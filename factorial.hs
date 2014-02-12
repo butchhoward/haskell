9a48abbb7b571d0ade13c4a9c1e8a82a9c3706fa
Butch Howard <bhoward@pillartechnology.com>
2014-02-11 22:11:57 -0600

    factorial flavors clean up

--- factorial.hs
+++ factorial.hs
@@ -5,19 +5,3 @@ factorial :: Integer -> Integer
 ---- Using recursion
 factorial 0 = 1
 factorial n | n > 0 = n * factorial (n - 1)
- 
----- Using recursion but written without pattern matching
----- factorial n = if n > 0 then n * factorial (n-1) else 1
- 
----- Using a list
----- factorial n = product [1..n]
- 
----- Using fold (implements product)
----- factorial n = foldl1 (*) [1..n]
- 
--- Point-free style
--- factorial = foldr (*) 1 . enumFromTo 1
- 
----- Point-free style analytic solution
---import Math.Gamma
---factorial = round . exp . lnGamma . fromIntegral . (+1)
