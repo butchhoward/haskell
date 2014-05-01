f8be80b0bee321313a2f300d2a7b7d99462cfc3a
Butch Howard <bhoward@pillartechnology.com>
2014-05-01 16:02:38 -0500

    experiment with runcmd

--- runcmd.hs
+++ runcmd.hs
@@ -2,19 +2,31 @@ import System.IO
 import System.Process
 
 main = do
-    (_, Just hout, Just herr, jHandle) <-
-        -- Replace with some other command on Windows
-        createProcess (proc "ls" ["-l", "/Users/"])
-           { cwd = Just "."
-           , std_out = CreatePipe
-           , std_err = CreatePipe 
-           }
+  
+--main = do
+--    (_, Just hout, Just herr, jHandle) <-
+--        -- Replace with some other command on Windows
+--        createProcess (proc "ls" ["-l", "/Users/"])
+--           { cwd = Just "."
+--           , std_out = CreatePipe
+--           , std_err = CreatePipe 
+--           }
 
-    putStrLn "stdout:"
-    s <- hGetContents hout
-
-    let sl = lines s
-    putStrLn $ "The Lines:" ++ show sl
+--    s <- hGetContents hout
+--    let sl = lines s
+--    putStrLn $ "The Lines:" ++ show sl
     
-    exitCode <- waitForProcess jHandle
-    putStrLn $ "Exit code: " ++ show exitCode
+--    exitCode <- waitForProcess jHandle
+--    putStrLn $ "Exit code: " ++ show exitCode
+
+doCmd :: String -> [String] -> [String]
+doCmd :: c, a = do
+  (_, Just hout, Just herr, jHandle) <-
+    -- Replace with some other command on Windows
+    createProcess (proc c a)
+       { cwd = Just "."
+       , std_out = CreatePipe
+       , std_err = CreatePipe 
+       }
+  s <- hGetContents hout
+  let sl = lines s
